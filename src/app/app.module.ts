d8066d663f7e6e23eca62b0e3f3e55f9239b3048
chris.krezias <chris.krezias@gmail.com>
2019-08-23 22:17:22 +0300

    feat: navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,34 @@ import { NewsComponent } from './views/news/news.component';
 import { PaypalTransferComponent } from './views/paypal-transfer/paypal-transfer.component';
 import { ContactInfoComponent } from './views/contact-info/contact-info.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NavMenuComponent } from './ui/nav-menu/nav-menu.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     PaypalTransferComponent,
-    ContactInfoComponent
+    ContactInfoComponent,
+    NavMenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ui/nav-menu/nav-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.scss']
+})
+export class NavMenuComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      share()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
